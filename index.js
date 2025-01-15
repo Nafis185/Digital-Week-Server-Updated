fd8a122e329b5c7d8775e96e2237387c00b03fa1
Nafis185 <nafisfuad185@gmail.com>
2025-01-15 12:48:24 +0600

    connect with client side

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,6 +27,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const eventsCollection = client.db("DigitalWeek").collection("Events");
+    // const userCollection = client.db("DigitalWeek").collection("Events");
+
+app.get('/Events', async (req, res) => {
+  const cursor = eventsCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+  })
+
+  app.get('/events/:id', async (req, res) => {
+    const id = req.params.id;
+    // const query = {_id: new ObjectId(id)   };
+    const query = {_id: new  ObjectId(id) };
+
+     const options = {
+       // Include only the `title` and `imdb` fields in the returned document
+       projection: {  title: 1, price: 1, _id: 1  },
+     };
+
+    const result = await eventsCollection.findOne(query , options);
+    res.send(result);
+  })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -34,7 +59,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
